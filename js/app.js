754e26a55e1dc4585b4be5c4fbb3e857b3b4208d
Olga Prudnik <80707801+OlgaPrudnik@users.noreply.github.com>
2021-10-12 00:29:24 +0300

    Create project

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector('.scheme-svg')
 const totalPriceTag = document.querySelector('.price-total')
+const menuButton = document.querySelector('.m-menu')
+const menu = document.querySelector('.menu')
 let cost = 800;
 let totalPrice = 0;
 
@@ -10,4 +12,8 @@ schemeSvg.addEventListener('click', (event) => {
   totalPrice = totalSeats * cost
   totalPriceTag.textContent = totalPrice
   
+})
+
+menuButton.addEventListener('click', () => {
+menu.classList.toggle('is-open')
 })
